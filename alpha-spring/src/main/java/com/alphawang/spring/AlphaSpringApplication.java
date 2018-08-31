66bdc111e17c5194bfbfa022a7037300f22ebad3
Alpha <awang@coupang.com>
2018-08-31 23:29:59 +0800

    [spring] init

---
+++ alpha-spring/src/main/java/com/alphawang/spring/AlphaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.alphawang.alphaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class AlphaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSpringApplication.class, args);
+    }
+}
