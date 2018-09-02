6e17e61e282f13f7476d843da418ce4e17541798
Alpha <awang@coupang.com>
2018-09-02 21:33:32 +0800

    [mongo] morphia

---
+++ alpha-mongodb/src/main/java/com/alphawang/mongodb/morphia/MorphiaCRUD.java
@@ -0,0 +1,34 @@
+package com.alphawang.mongodb.morphia;
+
+import com.alphawang.mongodb.morphia.entity.Member;
+import com.mongodb.MongoClient;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Key;
+import org.mongodb.morphia.Morphia;
+
+/**
+ * https://mongodb.github.io/morphia/
+ */
+@Slf4j
+public class MorphiaCRUD {
+
+    public static void main(String[] args) {
+        Morphia morphia = new Morphia();
+        
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        Datastore datastore = morphia.createDatastore(mongoClient, "alpha-db");
+
+        Member member = Member.builder()
+            .objectId(new ObjectId("5b8be519ff5fe15344be5222"))
+            .name("alpha-morphia")
+            .age(21)
+            .addr("Zhoupu")
+            .build();
+
+        Key<Member> key = datastore.save(member);  // save collection.
+        log.error("key : {}", key);
+        log.error("key-id : {}", key.getId());
+    }
+}
--- alpha-mongodb/src/main/java/com/alphawang/mongodb/morphia/entity/Member.java
+++ alpha-mongodb/src/main/java/com/alphawang/mongodb/morphia/entity/Member.java
@@ -1,4 +1,18 @@
 package com.alphawang.mongodb.morphia.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Id;
+
+@Data
+@Builder
 public class Member {
+    
+    @Id
+    private ObjectId objectId;
+    
+    private String name;
+    private int age;
+    private String addr;
 }
