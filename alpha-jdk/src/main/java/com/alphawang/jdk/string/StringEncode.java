5a1188997e39c3b5a243bc6cd2614493a07aac4a
Alpha <awang@coupang.com>
2018-09-05 20:47:03 +0800

    [jdk] encode

---
+++ alpha-jdk/src/main/java/com/alphawang/jdk/string/StringEncode.java
@@ -0,0 +1,48 @@
+package com.alphawang.jdk.string;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+@Slf4j
+public class StringEncode {
+
+    public static final String DEFAULT_CHARSET = "utf-8";
+
+    public static String encode(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        try {
+            return URLEncoder.encode(value, DEFAULT_CHARSET);
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    public static String decode(String value) {
+        try {
+            return URLDecoder.decode(value, DEFAULT_CHARSET);
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    public static void main(String[] args) {   
+        String msg = "Hello | World!";
+        log.info("origin {}", msg);
+        
+        String encoded = encode(msg);
+        log.info("encode {}", encoded);
+        
+        String decoded = decode(encoded);
+        log.info("decode {}", decoded);
+
+        String decoded2 = decode(decoded);
+        log.info("decode2 {}", decoded2);
+        
+    }
+}
