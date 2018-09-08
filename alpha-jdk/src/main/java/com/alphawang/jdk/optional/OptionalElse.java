fc382a014bcf34f78a9306b43261207920785cf1
Alpha <awang@coupang.com>
2018-09-08 19:30:19 +0800

    [jdk] Optional.ofNullable.map

---
+++ alpha-jdk/src/main/java/com/alphawang/jdk/optional/OptionalElse.java
@@ -0,0 +1,21 @@
+package com.alphawang.jdk.optional;
+
+import java.util.Optional;
+
+public class OptionalElse {
+
+    public static void main(String[] args) {
+        String result = Optional.ofNullable(Service.get())
+            .map(s -> s.toUpperCase())
+            .orElse("NULL");
+        
+        System.out.println(result);
+    }
+    
+    static class Service {
+        public static String get() {
+//            return null;
+            return "alpha";
+        }
+    }
+}
