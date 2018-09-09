76bf637df5afa21f5fd3b90025220c5d8e2f8c24
Alpha <awang@coupang.com>
2018-09-09 14:36:00 +0800

    [spring] remove to separate project

--- alpha-spring/src/main/java/com/alphawang/spring/scope/bean/SubSessionHolder.java
+++
@@ -1,9 +0,0 @@
-package com.alphawang.spring.scope.bean;
-
-import com.alphawang.spring.jar.SessionHolder;
-
-public class SubSessionHolder extends SessionHolder {
-    public void setCartSessionId(String cartSessionId) {
-        super.setCartSessionId(cartSessionId);
-    }
-}
