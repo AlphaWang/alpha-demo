bec1f6af1e4bf2e9304793e74b175a65ce97092c
Alpha <awang@coupang.com>
2018-10-04 20:41:37 +0800

    [redis] cluster script

---
+++ alpha-redis/src/main/resources/redis_3-cluster/run-cluster.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+redis-server conf/cluster-6379.conf &
+redis-server conf/cluster-6380.conf &
+redis-server conf/cluster-6381.conf &
+redis-server conf/cluster-6382.conf &
+redis-server conf/cluster-6383.conf &
+redis-server conf/cluster-6384.conf &
