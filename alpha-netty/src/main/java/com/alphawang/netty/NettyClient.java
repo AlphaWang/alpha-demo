4127c32654d41d2998540c2a9d006927c4b7fa70
Alpha <awang@coupang.com>
2018-10-26 23:48:32 +0800

    [netty] client

---
+++ alpha-netty/src/main/java/com/alphawang/netty/NettyClient.java
@@ -0,0 +1,45 @@
+package com.alphawang.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+public class NettyClient {
+
+    public static void main(String[] args) {
+
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        // ServerBootstrap --> Bootstrap
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+            // 线程模型
+            .group(workerGroup)
+            // IO模型
+            .channel(NioSocketChannel.class)
+            // IO处理逻辑
+            .handler(new ChannelInitializer<SocketChannel>() {
+                @Override 
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    System.out.println("===== client handler... " + ch);
+                }
+            });
+        
+        bootstrap
+            .connect("localhost", 8000)
+            .addListener(new GenericFutureListener<Future<? super Void>>() {
+                @Override 
+                public void operationComplete(Future<? super Void> future) throws Exception {
+                    if (future.isSuccess()) {
+                        System.out.println("连接成功");
+                    } else {
+                        System.err.println("连接失败");
+                    }
+                }
+            });
+    }
+}
