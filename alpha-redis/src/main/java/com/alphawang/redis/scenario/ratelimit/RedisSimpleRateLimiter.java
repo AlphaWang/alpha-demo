079d84dd93fe04d4dc58ef28180510e2cb33cc8f
Alpha <awang@coupang.com>
2018-11-07 00:17:02 +0800

    [redis] stop script

--- alpha-redis/src/main/java/com/alphawang/redis/scenario/ratelimit/RedisSimpleRateLimiter.java
+++ alpha-redis/src/main/java/com/alphawang/redis/scenario/ratelimit/RedisSimpleRateLimiter.java
@@ -30,12 +30,16 @@ public class RedisSimpleRateLimiter {
         long nowTs = System.currentTimeMillis();
         Pipeline pipe = jedis.pipelined();
         pipe.multi();
+        
         pipe.zadd(key, nowTs, "" + nowTs);
         pipe.zremrangeByScore(key, 0, nowTs - period * 1000);
+        
         Response<Long> count = pipe.zcard(key);
+        
         pipe.expire(key, period + 1);
         pipe.exec();
         pipe.close();
+        
         return count.get() <= maxCount;
     }
 
---
+++ alpha-redis/src/main/resources/redis_3-cluster/stop-cluster.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+redis-cli -p 6379 shutdown
+redis-cli -p 6380 shutdown
+redis-cli -p 6381 shutdown
+redis-cli -p 6382 shutdown
+redis-cli -p 6383 shutdown
+redis-cli -p 6384 shutdown
