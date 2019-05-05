d44f27718dbd16d9f9bc6c28e4cff6b8b5032054
AlphaWang <1919wang@gmail.com>
2019-05-05 17:28:10 +0800

    [alg] linked list cycle

---
+++ alpha-algorithm/src/main/java/com/alphawang/algorithm/leetcode/linkedlist/T141_LinkedListCycle.java
@@ -0,0 +1,71 @@
+package com.alphawang.algorithm.leetcode.linkedlist;
+
+public class T141_LinkedListCycle {
+    
+    /**
+     * Given a linked list, determine if it has a cycle in it.
+     *
+     */
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (true) {
+            if (slow == null || fast == null || fast.getNext() == null) {
+                return false;
+            }
+            
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+
+            if (slow != null && slow == fast) {
+                return true;
+            }
+        }
+    }
+
+    public static boolean hasCycle2(ListNode head) {
+        if (head == null || head.getNext() == null) {
+            return false;
+        }
+        
+        
+        ListNode slow = head;
+        ListNode fast = head.getNext();
+
+        while (slow != fast) {
+            if (slow == null || fast == null || fast.getNext() == null) {
+                return false;
+            }
+
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = ListNodeCreator.create(3, 2, 0, 4);
+        
+        ListNode toNode = null;
+        ListNode fromNode = null;
+        ListNode curr = node;
+        while (curr != null) {
+            if (curr.getValue().equals(2)) {
+                toNode = curr;
+            }
+            if (curr.getValue().equals(4)) {
+                fromNode = curr;
+            }
+
+            curr = curr.getNext();
+        }
+        
+        fromNode.setNext(toNode);
+        
+        System.out.println(hasCycle(node));
+        System.out.println(hasCycle2(node));
+    }
+    
+}
