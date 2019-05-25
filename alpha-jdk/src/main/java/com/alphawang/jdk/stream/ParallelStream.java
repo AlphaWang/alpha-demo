9593f14f706eb477de6064c0505a1c2dfb807174
AlphaWang <1919wang@gmail.com>
2019-05-26 00:18:02 +0800

    [lambda] parallel stream

---
+++ alpha-jdk/src/main/java/com/alphawang/jdk/stream/ParallelStream.java
@@ -0,0 +1,41 @@
+package com.alphawang.jdk.stream;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ParallelStream {
+    
+    private static final int N = 100;
+
+    public static void main(String[] args) {
+
+        /**
+         * Stream.parallel()
+         * Collection.parallelStream()
+         */
+
+        IntStream.range(0, N)
+            .parallel()
+            .mapToObj(i -> heavyCompute(i))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * main computing 717
+     * ForkJoinPool.commonPool-worker-2 computing 592
+     * ForkJoinPool.commonPool-worker-1 computing 561
+     * ForkJoinPool.commonPool-worker-6 computing 623
+     */
+    private static String heavyCompute(int number) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        String out = Thread.currentThread().getName() + " computing " + number;
+        System.out.println(out);
+        return out;
+    }
+}
