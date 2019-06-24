ca01b538dc5d70584f26c4d8fa4af24ee6d82145
AlphaWang <1919wang@gmail.com>
2019-06-24 18:48:24 +0800

    websocket init.

---
+++ alpha-websocket/src/main/java/com/alphawang/demo/alphawebsocket/AlphaWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.alphawang.demo.alphawebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaWebsocketApplication.class, args);
+    }
+
+}
---
+++ alpha-websocket/src/test/java/com/alphawang/demo/alphawebsocket/AlphaWebsocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alphawang.demo.alphawebsocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlphaWebsocketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
