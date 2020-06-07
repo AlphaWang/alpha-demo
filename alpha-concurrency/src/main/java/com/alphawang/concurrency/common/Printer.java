920a7c142176218e57bb01ea8f5fe75202936ef1
AlphaWang <awang@coupang.com>
2020-06-07 18:24:18 +0800

    [con] ParallelCollectors

--- alpha-concurrency/src/main/java/com/alphawang/concurrency/common/Printer.java
+++ alpha-concurrency/src/main/java/com/alphawang/concurrency/common/Printer.java
@@ -3,7 +3,7 @@ package com.alphawang.concurrency.common;
 public class Printer {
     
     public static void print(Object msg) {
-        System.out.println(String.format("[%s] - %s", Thread.currentThread().getName(), msg));
+        System.out.println(String.format("[%s] [%s] - %s", System.currentTimeMillis(), Thread.currentThread().getName(), msg));
     }
 
 }
