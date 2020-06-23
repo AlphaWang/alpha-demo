b387e81078bd4f9a63931c0f1f3d27234f4f1401
AlphaWang <1919wang@gmail.com>
2020-06-23 23:31:31 +0800

    47: Premutations2 - not removed duplication

--- alpha-algorithm/src/main/java/com/alphawang/algorithm/recursion/T0047_Permutations2.java
+++ alpha-algorithm/src/main/java/com/alphawang/algorithm/recursion/T0047_Permutations2.java
@@ -1,8 +1,11 @@
 package com.alphawang.algorithm.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -41,6 +44,42 @@ public class T0047_Permutations2 {
         }
     }
 
+    /**
+     * 2. 回溯，基于46
+     *    回溯，递归树：从空列表开始，依次放入
+     */
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> path = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+
+        Arrays.sort(nums);
+        dfs2(nums, nums.length, 0, used, path, res);
+        
+        return res;
+    }
+
+    private void dfs2(int[] nums, int length, int depth, boolean[] used, Deque<Integer> path, List<List<Integer>> res) {
+        // terminator
+        if (depth == length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for (int i = 0; i < length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            path.addLast(nums[i]);
+            used[i] = true;
+            
+            dfs2(nums, length, depth + 1, used, path, res);
+            
+            used[i] = false;
+            path.removeLast();
+        }
+    }
+
 
     public static void main(String[] args) {
         T0047_Permutations2 sut = new T0047_Permutations2();
@@ -54,7 +93,7 @@ public class T0047_Permutations2 {
           [2,1,1]
         ]
          */
-        System.out.println(sut.permuteUnique(new int[]{1,1,2}));
+        System.out.println(sut.permuteUnique2(new int[]{1,2,1}));
     }
     
 }
