a3417d7674abd82197b3a16ba776f506d7d5f69b
AlphaWang <1919wang@gmail.com>
2020-06-26 00:20:31 +0800

    T0025_ReverseNodesInKGroup

---
+++ alpha-algorithm/src/main/java/com/alphawang/algorithm/linkedlist/T0025_ReverseNodesInKGroup.java
@@ -0,0 +1,118 @@
+package com.alphawang.algorithm.linkedlist;
+
+/**
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/
+ * Hard
+ * 
+ */
+public class T0025_ReverseNodesInKGroup {
+
+    /**
+     * 1. 递归: 
+     *    先遍历K个元素，同时swap;
+     *    再将tail.next指向下一个递归
+     * 
+     *    0ms - 100%
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode next = head.next;
+        ListNode curr = head;
+        ListNode tail = head;
+        
+        ListNode tmp = head;
+        // 如果子链表不足K，则无需reverse
+        for (int i = 1; i < k; i++) {
+            if (tmp.next == null) {
+                return tail;
+            }
+            tmp = tmp.next;
+        }
+        // reverse 子链表
+        for (int i = 1; i < k; i++) {
+            ListNode newNext = next.next;
+            next.next = curr;
+            
+            curr = next;
+            next = newNext;
+        }
+        
+        tail.next = reverseKGroup(next, k);
+        
+        return curr;
+    }
+
+    /**
+     * 1_1. 递归: 优化swap子链表
+     *    先遍历K个元素，同时swap;
+     *    再将tail.next指向下一个递归
+     *
+     *    0ms - 100%
+     */
+    public ListNode reverseKGroup1_1(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode start = head;
+        ListNode end = head;
+        ListNode next = head;
+
+        for (int i = 1; i < k; i++) {
+            end = end.next;
+            if (end == null) {
+                end = start;
+                break;
+            }
+        }
+        next = end.next;
+        
+        ListNode reversed = reverse(start, end);
+        start.next = reverseKGroup1_1(next, k);
+        
+        return reversed;
+    }
+    
+    public ListNode reverse(ListNode start, ListNode end) {
+        ListNode next = start.next;
+        start.next = null;
+        
+        while (start != end) {
+            ListNode newNext = next.next;
+            next.next = start;
+            
+            start = next;
+            next = newNext;
+        }
+        
+        return end;
+    }
+
+    public static void main(String[] args) {
+        T0025_ReverseNodesInKGroup sut = new T0025_ReverseNodesInKGroup();
+        ListNode node = ListNodeCreator.create(1, 2, 3, 4, 5);
+        ListNode end = node;
+        for (int i = 0; i < 4; i++) {
+            end = end.next;
+        }
+        System.out.println(ListNode.format(sut.reverse(node, end)));
+        System.out.println("------");
+        
+        /*
+                  Given this linked list: 1->2->3->4->5
+            For k = 2, you should return: 2->1->4->3->5
+            For k = 3, you should return: 3->2->1->4->5
+         */
+        node = ListNodeCreator.create(1, 2, 3, 4, 5);
+        System.out.println(ListNode.format(sut.reverseKGroup1_1(node, 2)));
+
+        node = ListNodeCreator.create(1, 2, 3, 4, 5);
+        System.out.println(ListNode.format(sut.reverseKGroup1_1(node, 3)));
+        
+        
+    }
+
+}
