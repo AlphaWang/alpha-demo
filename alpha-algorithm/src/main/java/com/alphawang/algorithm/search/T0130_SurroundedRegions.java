8a3ed0ad80950f3c7cbf1977852149ac928cb6b0
AlphaWang <awang@coupang.com>
2020-07-24 23:37:59 +0800

    T0130_SurroundedRegions: UnionFind

--- alpha-algorithm/src/main/java/com/alphawang/algorithm/search/T0130_SurroundedRegions.java
+++ alpha-algorithm/src/main/java/com/alphawang/algorithm/search/T0130_SurroundedRegions.java
@@ -68,6 +68,95 @@ public class T0130_SurroundedRegions {
         return i == 0 || i == m - 1 || j == 0 || j == n - 1;
     }
 
+    /**
+     * 2. 并查集
+     * 
+     *    靠近边界的 O, 都让其从属与一个 dummyNode
+     *    其余的O，与相邻O组成集合
+     *    
+     *    24ms - 5%
+     */
+    public void solve2(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+        
+        JointSet jointSet = new JointSet(m * n + 1);
+        int dummy = m * n;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    int current = encode(i, j, n);
+                    if (isEdge(i, j, m, n)) {
+                        jointSet.join(current, dummy);
+                    } else {
+                        if (i > 0 && board[i - 1][j] == 'O') {
+                            jointSet.join(encode(i - 1, j, n), current);
+                        }
+                        if (i < m - 1 && board[i + 1][j] == 'O') {
+                            jointSet.join(encode(i + 1, j, n), current);
+                        }
+                        if (j > 0 && board[i][j - 1] == 'O') {
+                            jointSet.join(encode(i, j - 1, n), current);
+                        }
+                        if (j < n - 1 && board[i][j + 1] == 'O') {
+                            jointSet.join(encode(i, j + 1, n), current);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O' && jointSet.find(encode(i, j, n)) != jointSet.find(dummy)) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        
+    }
+    
+    private int encode(int i, int j, int n) {
+        return i * n + j;
+    }
+
+
+    private static class JointSet {
+        int n;
+        int[] parent;
+        
+        public JointSet(int n) {
+            this.n = n;
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+        
+        public int find(int p) {
+            while(p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            
+            return p;
+        }
+        
+        public void join(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP != rootQ) {
+                parent[rootP] = rootQ;
+            }
+        }
+        
+    }
+
     public static void main(String[] args) {
         test(new char[][] {
           {'X', 'X', 'X', 'X'},
@@ -75,12 +164,21 @@ public class T0130_SurroundedRegions {
           {'X', 'X', 'O', 'X'},
           {'X', 'O', 'X', 'X'}
         });
+        
+        test(new char[][]{
+          {'X','O','X','X'},
+          {'O','X','O','X'},
+          {'X','O','X','O'},
+          {'O','X','O','X'},
+          {'X','O','X','O'},
+          {'O','X','O','X'}
+        });
     }
     
     private static void test(char[][] board) {
         T0130_SurroundedRegions sut = new T0130_SurroundedRegions();
         System.out.println(Arrays.deepToString(board));
-        sut.solve(board);
+        sut.solve2(board);
         System.out.println(Arrays.deepToString(board));
     }
 
