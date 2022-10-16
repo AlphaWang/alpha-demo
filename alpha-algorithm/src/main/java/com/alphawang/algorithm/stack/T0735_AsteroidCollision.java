8c2d81e5a10af0e1971360ca34895eab8325679b
zhongxwang <zhongxwang@ebay.com>
2022-10-16 23:11:24 +0800

    T0735_AsteroidCollision: fix

--- alpha-algorithm/src/main/java/com/alphawang/algorithm/stack/T0735_AsteroidCollision.java
+++ alpha-algorithm/src/main/java/com/alphawang/algorithm/stack/T0735_AsteroidCollision.java
@@ -4,6 +4,8 @@ import static com.alphawang.algorithm.Utils.print;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/asteroid-collision/
@@ -14,7 +16,44 @@ import java.util.Deque;
  */
 public class T0735_AsteroidCollision {
 
+  /**
+   * 更直观
+   * overall, there are totally 4 scenarios will happen: 1.+ + 2.- - 3.+ - 4.- +
+   * when collision happens: only 3 which is + -
+   * use a stack to keep track of the previous and compare current value with previous ones
+   */
   public int[] asteroidCollision(int[] a) {
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int item : a) {
+      if (item > 0) {
+        // 1.++ 4.-+: push
+        stack.push(item);
+      } else {
+        // 3.+-: 如果当前值大，则 peek collides.
+        // destroy previous positive ones
+        while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -item) {
+          stack.pop();
+        }
+
+        if (stack.isEmpty() || stack.peek() < 0) {
+          // 2.--: push
+          stack.push(item);
+        } else if (stack.peek() == -item) {
+          // 3.+-: 如果值相同，则 both collides.
+          stack.pop();
+        }
+      }
+    }
+
+    int[] res = new int[stack.size()];
+    for (int i = stack.size() - 1; i >= 0; i--) {
+      res[i] = stack.pop();
+    }
+    return res;
+  }
+
+  // bug: cannot handle [-8, 8]
+  public int[] asteroidCollision1(int[] a) {
     int n = a.length;
     Deque<Integer> stack = new ArrayDeque<>();
     stack.push(a[0]);
@@ -48,10 +87,55 @@ public class T0735_AsteroidCollision {
     return res;
   }
 
+  public int[] asteroidCollision2(int[] asteroids) {
+    Stack<Integer> stack = new Stack();
+    for (int ast: asteroids) {
+      collision: {
+        while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
+          if (stack.peek() < -ast) {
+            stack.pop();
+            continue;
+          } else if (stack.peek() == -ast) {
+            stack.pop();
+          }
+          break collision;
+        }
+        stack.push(ast);
+      }
+    }
+
+    int[] ans = new int[stack.size()];
+    for (int t = ans.length - 1; t >= 0; --t) {
+      ans[t] = stack.pop();
+    }
+    return ans;
+  }
+
+  public int[] asteroidCollision4(int[] a) {
+    LinkedList<Integer> s = new LinkedList<>();
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] > 0 || s.isEmpty() || s.getLast() < 0) {
+        s.add(a[i]);
+      } else if (s.getLast() <= -a[i]) {
+        if (s.pollLast() < -a[i])
+          i--;
+      }
+    }
+    return s.stream().mapToInt(i->i).toArray();
+  }
+
   public static void main(String[] args) {
     T0735_AsteroidCollision sut = new T0735_AsteroidCollision();
-    print(sut.asteroidCollision(new int[]{10, 2, -5})); //[10]
-    print(sut.asteroidCollision(new int[]{5, 10, -5})); //[5, 10]
-    print(sut.asteroidCollision(new int[]{8, -8})); //[]
+    sut.test(new int[]{10, 2, -5}); //[10]
+    sut.test(new int[]{5, 10, -5}); //[5, 10]
+    sut.test(new int[]{-8, 8}); //[] //应该返回 [-8, 8] !!!负数表示往左！
+    sut.test(new int[]{8, -8}); //[]
+  }
+
+  private void test(int[] a) {
+//    print(asteroidCollision1(a));
+//    print(asteroidCollision2(a));
+    print(asteroidCollision(a));
+//    print(asteroidCollision4(a));
   }
 }
