598a886f3c3631226fac1610f958ac7e5f3de75b
zhongxwang <zhongxwang@ebay.com>
2022-12-26 22:28:54 -0800

    kafka source demo

---
+++ alpha-flink/src/main/java/com/alpha/demo/flink/kafka/KafkaSourceDemo.java
@@ -0,0 +1,47 @@
+package com.alpha.demo.flink.kafka;
+
+import java.util.Properties;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.LocalEnvironment;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class KafkaSourceDemo {
+
+  public static void main(String[] args) throws Exception {
+
+    String brokers = "localhost:9092";
+    String topic = "test";
+
+    Configuration conf = new Configuration();
+    conf.setString("heartbeat.timeout", "18000000");
+    conf.setString("akka.ask.timeout", "18000000");
+//    LocalEnvironment env = ExecutionEnvironment.createLocalEnvironment(conf);
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+
+
+    KafkaSource<String> source = KafkaSource.<String>builder()
+        .setBootstrapServers(brokers)
+        .setTopics(topic)
+        .setGroupId("alpha-flink-group")
+        .setStartingOffsets(OffsetsInitializer.earliest())
+        .setValueOnlyDeserializer(new SimpleStringSchema())
+        .build();
+
+    DataStream<String> messageStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Alpha Kafka Source");
+    messageStream.rebalance().map(new MapFunction<String, String>() {
+      @Override
+      public String map(String value) throws Exception {
+        return "Kafka and Flink says: " + value;
+      }
+    }).print();
+
+    env.execute();
+  }
+}
